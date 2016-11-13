c6f1c5d3935dd0790a1e8bc0042c50f210567eca
manubocquet <manu.bocquet@gmail.com>
2016-11-13 17:32:23 -0500

    First release

---
+++ init.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+
+link () {
+        source="$1"
+        dest="$2"
+        if [ -f "${source}" ] || [ -d "${source}" ] ; then
+                if [ ! -e "${dest}" ] ; then
+                        if [ -f "${source}" ]; then
+                                rpath=$(dirname "${dest}")
+                                if [ ! -d "${rpath}" ]; then
+                                        mkdir -p "${rpath}"
+                                fi
+                        fi
+                        mv "${source}" "${dest}"
+                else
+                        rm -rf "${source}"
+                fi
+        fi
+        ln -s "${dest}" "${source}"
+}
+
+# config
+link "/etc/mosquitto" "/config/etc/mosquitto"
+
+# /etc/syslog-ng/syslog-ng.conf
+link "/etc/syslog-ng/syslog-ng.conf" "/config/etc/syslog-ng.conf"
+
+mkdir -p /config/log
+
+# Launch mosquito
+exec /usr/local/sbin/mosquitto >/config/log/mosquitto.log 2>&1
---
+++ launch.sh
@@ -0,0 +1,1 @@
+docker run -d --name="mosquitto" -h mosquitto -v /mnt/user/appdata/mosquitto:/config -p 1883:1883 mosquitto
